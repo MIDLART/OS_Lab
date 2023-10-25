5dbeb75ee2a1e9a816920ad6c30022bee253f398
MIDLART <145492479+MIDLART@users.noreply.github.com>
2023-10-25 20:52:51 +0300

    Add files via upload

---
+++ Lab2/OS2_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main () {
+	pid_t pid, ppid, pgrp, uid, gid, euid, egid;
+	
+	pid = getpid();
+	ppid = getppid();
+	pgrp = getpgrp();
+	uid = getuid();
+	gid = getgid();
+	euid = geteuid();
+	egid = getegid();
+	
+	printf("Идентификатор текущего процесса: %d\n", pid);
+	printf("Идентификатор родительского процесса: %d\n", ppid);
+	printf("Идентификатор группы вызывающего процесса: %d\n", pgrp);
+	printf("Реальный идентификатор владельца: %d\n", uid);
+	printf("Реальный идентификатор группы владельца: %d\n", gid);
+	printf("Эффективные идентификаторы владельца: %d\n", euid);
+	printf("Эффективные идентификаторы группы владельца: %d\n", egid);
+
+	return 0;
+}
